581e98b683016ce618a5e7118f7462590aa02e49
zealic <zealic@gmail.com>
2011-12-21 19:19:15 +0800

    - Remove SpellFlash.

--- src/do_get.py
+++ src/do_get.py
@@ -103,18 +103,6 @@ if __name__ == "__main__":
       "reforgelite",               # ReforgeLite
       "silver-dragon",             # SilverDragon
       "snoopy-inspect",            # Snoopy Inspect : 超出距离依然能查看
-      "spellflash",                # SpellFlash
-      "spellflash_warlock",        # SpellFlash: Warlock
-      "spellflash_hunter",         # SpellFlash: Hunter
-      "spellflash_warrior",        # SpellFlash: Warrior
-      "spellflash_racials",        # SpellFlash: Racials
-      "spellflash_druid",          # SpellFlash: Druid
-      "spellflash-priest",         # SpellFlash: Priest
-      "spellflash_mage",           # SpellFlash: Mage
-      "spellflash_paladin",        # SpellFlash: Paladin
-      "spellflash-death-knight",   # SpellFlash: Death Knight
-      "spellflash_healadin",       # SpellFlash: Healadin
-      "spellflash_rogue",          # SpellFlash: Rogue
       "xperl",                     # X-Perl UnitFrame
       "coordinates",               # Coordinates
       "inspect-equip",             # InspectEquip
