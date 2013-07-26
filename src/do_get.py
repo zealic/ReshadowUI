72e04c70b7ea2a8ef424c901e7afb1ceadf5680f
zealic <zealic@gmail.com>
2013-07-26 12:46:21 +0800

    + Use TaskManager management download task.

--- src/do_get.py
+++ src/do_get.py
@@ -40,31 +40,31 @@ def get_wowinterface_download_info(name, id):
     result.link = url
     return result
 
-def get_package_core(task):
-    message = "[%s] DONE!" % (task.name)
+def get_package_core(name, id , fetcher):
+    message = "DONE!"
     try:
-        log("[%s] Fetching '%s' download info..." % (task.name, task.id))
-        info = task.fetcher(task.name, task.id)
+        log("[%s] Fetching '%s' download info..." % (name, id))
+        info = fetcher(name, id)
         download_link = info.link
         base_name = None
         if info.attachment:
             base_name = info.attachment.name
         if download_link == None:
-            log("[%s] No download link!" % (task.name))
+            log("[%s] No download link!" % (name))
         else:
-            log("[%s] Downloading '%s' ..." % (task.name, download_link))
+            log("[%s] Downloading '%s' ..." % (name, download_link))
             download_file(download_link, os.getcwd(), base_name)
     except Exception, e:
-        message = "[%s] FAILED, %s" % (task.name, str(e))
-    return message
+        return -1, "FAILED, %s" % (str(e),)
+    return None, message
 
 def new_task(name, id, fetcher):
-    from StringIO import StringIO
-    task = StringIO()
-    task.name = name
-    task.id = id
-    task.fetcher = fetcher
-    return task
+    def _task_core():
+      return get_package_core(name, id, fetcher)
+    _task_core.name = name
+    _task_core.id = id
+    _task_core.fetcher = fetcher
+    return _task_core
 
 def load_configuration(config_file):
   import json
@@ -87,29 +87,47 @@ def get_tasks():
             tasks.append(new_task(name, addons[name], fetcher))
     return tasks
 
-def job_core(tasks):
-    while tasks.qsize() > 0:
-        task = tasks.get()
-        msg  = get_package_core(task)
-        log(">> [%s] : %s" % (task.name, msg))
-        if tasks.qsize() != 0:
-            log("   Reaming %d." % (tasks.qsize()))
-        tasks.task_done()
-
-def start_jobs(tasks):
-    JOBS_SIZE = 5
-    from threading import Thread
-    for i in xrange(1, JOBS_SIZE):
-        runner = Thread(target=job_core, args = (tasks,))
-        runner.start()
-
-if __name__ == "__main__":
+class TaskManager(object):
+  import threading
+  lock = threading.Lock()
+  
+  def run(self, tasks):
+    import time
     from Queue import Queue
+    from threading import Thread
+
+    def _job_core(tasks):
+      while tasks.qsize() > 0:
+        if self.lock.acquire():
+          task = tasks.get()
+          self.lock.release()
+          code, msg  = task()
+          log(">> [%s] : %s" % (task.name, msg))
+          if isinstance(code, int): exit(code)
+          if tasks.qsize() != 0:
+              log("   Reaming %d." % (tasks.qsize()))
+          tasks.task_done()
+    
+    JOBS_SIZE = 5
     runningTasks = Queue()
-    for task in get_tasks():
+    # Run tasks with job size
+    for task in tasks:
         runningTasks.put(task)
     
-    start_jobs(runningTasks)
+    runners = []
+    for i in xrange(JOBS_SIZE):
+        runner = Thread(target = _job_core, args = (runningTasks,))
+        runner.daemon = True
+        runner.start()
+        runners.append(runner)
+    # Wait all tasks complete, It can response Ctrl + C interrupt.
+    while any(runner.isAlive() for runner in runners):
+      time.sleep(1)
     runningTasks.join()
+
+
+if __name__ == "__main__":
+    taskMan = TaskManager()
+    taskMan.run(get_tasks())
     
     log("ALL DONE!")
